edba126bd5e0892cb7be269bedc1135d0bc64ab7
Teguh <teguhbadrusalam6@gmail.com>
2024-12-03 06:40:36 +0700

    update path api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,11 +9,6 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  // @Post('tweet')
-  // async createTweet(@Body() tweetData: { content: string }) {
-  //   return await this.appService.autoPostToTwitter(tweetData.content);
-  // }
 
   @Get('test-mp3')
   async testMP3() {
