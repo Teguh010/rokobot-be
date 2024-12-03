deae5df97d997d9ce1e99aef2c0a37c1e278ac40
Teguh <teguhbadrusalam6@gmail.com>
2024-12-04 06:57:33 +0700

    update

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { UpdateTweetContentColumn } from './migrations/UpdateTweetContentColumn'
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3306,
   username: 'rokobot_user1',
   password: 'BisMillah22x#',
