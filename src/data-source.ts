d6d7d5a4908fe612416badb820511a3bcb5d8c8f
Teguh <teguhbadrusalam6@gmail.com>
2024-12-04 07:21:04 +0700

    update data soruce and migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,18 @@
 import { DataSource } from 'typeorm'
 import { Tweet } from './entities/tweet.entity'
-import { UpdateTweetContentColumn } from './migrations/UpdateTweetContentColumn'
+import { UpdateTweetContentColumn1701648000000 } from './migrations/1701648000000-UpdateTweetContentColumn'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'rokobot_user1',
-  password: 'BisMillah22x#',
-  database: 'rokobot',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME || 'rokobot_user1',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'rokobot',
   entities: [Tweet],
-  migrations: [UpdateTweetContentColumn],
+  migrations: [UpdateTweetContentColumn1701648000000],
   synchronize: false,
 })
